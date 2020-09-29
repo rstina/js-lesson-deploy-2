78e40f25410daa15fdbe15e373e7b5abb7b5e2c9
rstina <53047495+rstina@users.noreply.github.com>
2020-09-29 10:36:43 +0200

    Updates

---
+++ precache-manifest.423a643589245c4424a8707dc48c7e6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2e2a632b38f6b9e0a69b46a34f9bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a1b44b11ffbb81697b8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ebfa5ae144bc2a4ef1c3",
+    "url": "/static/js/2.8d7946c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8d7946c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a1b44b11ffbb81697b8",
+    "url": "/static/js/main.6e10093a.chunk.js"
+  },
+  {
+    "revision": "c2f1184d555aa62a788a",
+    "url": "/static/js/runtime-main.175043e2.js"
+  }
+]);
